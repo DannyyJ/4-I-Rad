8322319d2e1dfc640ce73edbcf562ca5d119ff4f
Daniel <daniel.landqvistjacobsen@skola.taby.se>
2025-05-12 10:50:18 +0200

    fungerar

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, '../frontend')));
 app.use(express.json());
-
+//HEj
 const games = {};
 
 io.on('connection', (socket) => {
